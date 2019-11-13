6ea6ea4234b9fdd7b108724d0a91a5bc627bc551
Zach <zach@sprinterconsulting.com>
2019-11-13 17:39:07 -0600

    updated contact form

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { navigate } from 'gatsby-link'
 import Layout from '../../components/Layout'
+import { MapPin, Smartphone, Mail } from 'react-feather'
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 
@@ -14,7 +15,7 @@ export default class Index extends React.Component {
   constructor(props) {
     super(props)
     this.state = { isValidated: false }
-  }
+  }gat
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value })
@@ -45,81 +46,155 @@ export default class Index extends React.Component {
               marginTop: 2,
             }}>
               Contact
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{' '}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
+              <div class="columns">
+                <div className='contactInformation column'
+                sx={{
+                  padding: 6,
+                }}>
+                    <a
+                      className="tile"
+                      sx={{
+                        margin: 4,
+                      }}
+                      // href={`https://www.google.com.au/maps/search/${encodeURI(
+                      //   address
+                      // )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <MapPin 
+                      sx={{
+                      marginRight: 3,
+                      }}
+                      /> 9005 Overlook Blvd
+                    </a>
+                    <a className="tile"
+                    sx={{
+                      margin: 4,
+                    }}>
+                      <Smartphone 
+                      sx={{
+                      marginRight: 3,
+                      }}
+                      /> +1 000 000 0000
+                    </a>
+                    <a className="tile"
+                    sx={{
+                      margin: 4,
+                    }}>
+                      <Mail 
+                      sx={{
+                      marginRight: 3,
+                      }}
+                        /> example@example.com
+                    </a>
                 </div>
-                <div className="field">
-                  <label className="label" htmlFor={'name'}
-                  sx={{
-                    color: "text"
-                  }}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'text'}
-                      name={'name'}
-                      onChange={this.handleChange}
-                      id={'name'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'email'}
-                  sx={{
-                    color: 'text',
-                  }}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'email'}
-                      name={'email'}
-                      onChange={this.handleChange}
-                      id={'email'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'message'}
-                  sx={{
-                    color: 'text',
-                  }}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={'message'}
-                      onChange={this.handleChange}
-                      id={'message'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
+                <div className="column">
+                    <form
+                      name="contact"
+                      method="post"
+                      action="/contact/thanks/"
+                      data-netlify="true"
+                      data-netlify-honeypot="bot-field"
+                      onSubmit={this.handleSubmit}
+                    >
+                      {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                      <input type="hidden" name="form-name" value="contact" />
+                      <div hidden>
+                        <label>
+                          Don’t fill this out:{' '}
+                          <input name="bot-field" onChange={this.handleChange} />
+                        </label>
+                      </div>
+                      <div className="field">
+                        <label className="label" htmlFor={'name'}
+                        sx={{
+                          color: "text"
+                        }}>
+                          Your name
+                        </label>
+                        <div className="control">
+                          <input
+                            className="input"
+                            type={'text'}
+                            name={'name'}
+                            onChange={this.handleChange}
+                            id={'name'}
+                            required={true}
+                          />
+                        </div>
+                      </div>
+                      <div className="field">
+                        <label className="label" htmlFor={'email'}
+                        sx={{
+                          color: 'text',
+                        }}>
+                          Email
+                        </label>
+                        <div className="control">
+                          <input
+                            className="input"
+                            type={'email'}
+                            name={'email'}
+                            onChange={this.handleChange}
+                            id={'email'}
+                            required={true}
+                          />
+                        </div>
+                      </div>
+                      <div className="field">
+                        <label className="label" htmlFor={'enquiry'}
+                        sx={{
+                          color: 'text',
+                        }}>
+                          Type of Enquiry
+                        </label>
+                        <div className="control">
+                          <select 
+                          className="select"
+                          type={'enquiry'}
+                          name={'enquiry'}
+                          onChange={this.handleChange}
+                          id={'enquiry'}
+                          required={false}>
+                            <option disabled>
+                              Select One
+                            </option>
+                            <option>Need to know more</option>
+                            <option>Want to say hello!</option>
+                            <option>Found a bug</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="field">
+                        <label className="label" htmlFor={'message'}
+                        sx={{
+                          color: 'text',
+                        }}>
+                          Message
+                        </label>
+                        <div className="control">
+                          <textarea
+                            className="textarea"
+                            name={'message'}
+                            onChange={this.handleChange}
+                            id={'message'}
+                            required={true}
+                          />
+                        </div>
+                      </div>
+                      <div className="field">
+                        <button className="button is-link" type="submit">
+                          Send
+                        </button>
+                      </div>
+                      <div
+                      className="g-recaptcha"
+                      data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
+                      />
+                    </form>
+                    </div>
+            </div>
             </div>
           </div>
         </section>
