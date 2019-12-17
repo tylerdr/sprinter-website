1789a9103246cdb6b121cc62fb96902a80fda71f
Zach <zach@sprinterconsulting.com>
2019-12-17 16:40:23 -0600

    updated thanks margins, added widgets for flexibility  of about format

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 export default () => (
   <Layout>
     <section className="section">
       <div className="container">
         <div className="content">
-          <h3>Thank you for reaching out. We know your time is valuable and appreciate every second that went into your message. We will be sure to respond as promptly as possible. 
--Sprinter</h3>
+          <h3
+          sx={{
+             margin: 5,
+            }}
+            >Thank you for reaching out. We know your time is valuable and appreciate every second that went into your message. We will be sure to respond as promptly as possible. 
+            -Sprinter
+          </h3>
         </div>
       </div>
     </section>
