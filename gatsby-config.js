93560f2a41bd98625fe92f78ef5ee9970bd188a3
Zach <zach@sprinterconsulting.com>
2020-01-15 22:20:25 -0600

    im dumb

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/img/sprinter-favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
