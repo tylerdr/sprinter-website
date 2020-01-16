bbf475a9c02eda54aae1aa3322847a8da962ae41
Zach <zach@sprinterconsulting.com>
2020-01-15 22:00:31 -0600

    gatsby plugin manfiest

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/img`,
