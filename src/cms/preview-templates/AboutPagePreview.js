a7835ac463b2708937880706955d2cfe0fa13644
Tyler Dreher <tyler@sprinterconsulting.com>
2020-04-10 01:50:05 -0500

    Fixed local admin issues & about page cms preview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -4,23 +4,23 @@ import { AboutPageTemplate } from '../../templates/about-page'
 
 const AboutPagePreview = ({ entry }) => { 
   const data = entry.getIn(['data']).toJS()
-  {console.log(data)}
+  console.log(data,'data')
   if (data) {
     return (
         <AboutPageTemplate
           title={data.title}
-          headerImage={data.headerImage}
           subtitle={data.subtitle}
-          what={data.what}
-          how={data.how}
-          why={data.why}
-          coreValues={data.coreValues}
+          headerImage={data.headerImage}
           imageA={data.imageA}
           imageB={data.imageB}
           imageC={data.imageC}
           imageD={data.imageD}
           coreValuesTest={data.coreValuesTest}
-        />
+          whatSlide={data.whatSlide}
+          howSlide={data.howSlide}
+          whySlide={data.whySlide}
+          blockquotes={data.blockquotes}
+          />
     )
     } else {
       return <div>Loading...</div>
@@ -36,3 +36,20 @@ AboutPagePreview.propTypes = {
 }
 
 export default AboutPagePreview
+
+
+{/* <Layout>
+<AboutPageTemplate
+  title={frontmatter.title}
+  subtitle={frontmatter.subtitle}
+  headerImage={frontmatter.headerImage}
+  imageA={frontmatter.imageA}
+  imageB={frontmatter.imageB}
+  imageC={frontmatter.imageC}
+  imageD={frontmatter.imageD}
+  coreValuesTest={frontmatter.coreValuesTest}
+  whatSlide={frontmatter.whatSlide}
+  howSlide={frontmatter.howSlide}
+  whySlide={frontmatter.whySlide}
+  blockquotes={frontmatter.blockquotes}/>
+</Layout> */}
