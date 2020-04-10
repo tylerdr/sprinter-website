f27e957bd3daa05fd2c2a0741517dbd99a2b1795
Zach <zach@sprinterconsulting.com>
2020-04-10 13:40:05 -0500

    fixed SSR issues

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
+import { Helmet } from "react-helmet"
 
 export default class TechStack extends Component {
-    componentWillMount () {
-        const script = document.createElement("script")
-        script.src = "https://cdn1.stackshare.io/javascripts/client-code.js"
-        script.async = true
-        document.body.appendChild(script);
-    }
-
     render() {
         return (
-            <div className="column is-12">
-                <a frameborder='0' data-theme='dark' data-layers='1,2,3,4' data-stack-embed='true' href='https://embed.stackshare.io/stacks/embed/f2bf411874c7dca75be20ffdbd6446'></a>
-            </div>
+            <React.Fragment>
+                <Helmet>
+                 <script async src='https://cdn1.stackshare.io/javascripts/client-code.js' charset='utf-8'></script>
+                </Helmet>
+                <div className="column is-12">
+                    <a frameborder='0' data-theme='dark' data-layers='1,2,3,4' data-stack-embed='true' href='https://embed.stackshare.io/stacks/embed/f2bf411874c7dca75be20ffdbd6446'></a>
+                </div>
+            </React.Fragment>
+            
         )
     }
 
