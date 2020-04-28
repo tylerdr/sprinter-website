3053e3ffbfeebb94b20ef030fed61f4c29d50bac
Zach <zach@sprinterconsulting.com>
2020-04-27 19:29:44 -0500

    Adjusted contact form and header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import { Link } from 'gatsby'
 import InstagramFeed from './InstagramFeed'
 import GoogleMap from './GoogleMap'
 import { SocialIcon } from 'react-social-icons';
+import { MapPin, Smartphone, Mail } from 'react-feather'
 
 const Footer = class extends React.Component {
   render() {
@@ -15,43 +16,85 @@ const Footer = class extends React.Component {
         backgroundColor: "primary",
       }}/>
       <footer className="footer">
-        {/*<div className="has-text-centered">*/}
-        {/*  <img*/}
-        {/*    src={logo}*/}
-        {/*    alt="Sprinter"*/}
-        {/*    style={{ width: '14em', height: '10em' }}*/}
-        {/*  />*/}
-        {/*</div>*/}
         <div>
-            {/* <div className="columns is-vcentered" 
+             <div className="columns is-vcentered" 
             sx={{
               marginRight: 4,
               marginLeft: 4,
-            }}> */}
-              {/* <div className="column">
-                <div className="tile"
-                sx={{
-                  color: "text",
-                  fontSize: 4,
-                }}>
-                  For questions, call us at 1-000-000-000 or email us at example@example.com.
-                </div>
-              </div> */}
-              {/* <div className="tile is-7"
+            }}> 
+               <div className="column">
+                 <Link to="/contact" className="tile" sx={{fontSize: 6, margin: 4}}>Say Hello</Link>
+                  <a
+                      className="tile"
+                      sx={{
+                        margin: 4,
+                      }}
+                      href={`https://www.google.com.au/maps/search/${encodeURI(
+                       "9005 Overlook Blvd"
+                      )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <MapPin 
+                      sx={{
+                      marginRight: 3,
+                      }}
+                      /> 
+                    <p 
+                    sx={{
+                      fontSize: 2,
+                      fontWeight: "body",
+                      fontFamily: "body",
+                    }}>9005 Overlook Blvd
+                    </p>
+                    </a>
+                    <a className="tile"
+                    href={`tel:${"+1-615-957-5603"}`}
+                    sx={{
+                      margin: 4,
+                    }}>
+                      <Smartphone 
+                      sx={{
+                      marginRight: 3,
+                      }}
+                      />
+                      <p
+                      sx={{
+                        fontSize: 2,
+                        fontWeight: "body",
+                        fontFamily: "body",
+                      }}>
+                       +1 (615) 601-0782
+                       </p>
+                    </a>
+                    <a className="tile"
+                    href={`mailto:${"hello@sprinterconsulting.com"}`}
+                    sx={{
+                      margin: 4,
+                    }}>
+                      <Mail 
+                      sx={{
+                      marginRight: 3,
+                      }}
+                        />
+                        <p 
+                        sx={{
+                          fontSize: 2,
+                          fontWeight: "body",
+                          fontFamily: "body",
+                        }}>
+                          hello@sprinterconsulting.com
+                        </p>
+                    </a>
+              </div> 
+               <div className="column is-7"
               sx={{margin:"auto"}}>
                 <GoogleMap />
-              </div> */}
-              {/* <div className="tile is-7" sx={{margin:"auto"}}>
-                <p
-                // sx={{
-                //         fontFamily: "body",
-                //         fontSize: 3,
-                //         padding: 3,
-                //         margin: 3,
-                //         fontWeight: "body",
-                // }}>Our passion for technology combined with our love of travel has allowed us to work in some of the most beautiful places in the world. The map to the left pinpoints all of our temporary offices where we’ve opened our laptops while enjoying breathtaking cities, scenery and culture.</p>
-              </div> */}
-            {/* </div> */}
+              </div> 
+               <div className="column" sx={{margin:"auto"}}>
+               <iframe src="https://sprinter.substack.com/embed" width="480" height="320" frameborder="0" scrolling="no"></iframe> 
+              </div> 
+             </div> 
             <div className="social-bar has-text-centered" >
             <SocialIcon url="https://instagram.com/sprinterconsulting/" 
             sx={{
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -5,7 +5,7 @@ import Helmet from 'react-helmet'
 import { MapPin, Smartphone, Mail } from 'react-feather'
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-
+import { ReactTypeformEmbed } from 'react-typeform-embed';
 
 function encode(data) {
   return Object.keys(data)
@@ -19,77 +19,28 @@ export default class Index extends React.Component {
     this.state = { isValidated: false }
   }
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+  // handleChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value })
+  // }
 
-  handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch(error => alert(error))
-  }
+  // handleSubmit = e => {
+  //   e.preventDefault()
+  //   const form = e.target
+  //   fetch('/', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  //     body: encode({
+  //       'form-name': form.getAttribute('name'),
+  //       ...this.state,
+  //     }),
+  //   })
+  //     .then(() => navigate(form.getAttribute('action')))
+  //     .catch(error => alert(error))
+  // }
 
   render() {
     return (
       <Layout sx={{color: "text"}}>
-        <div
-      className="full-width-image margin-top-0"
-        style={{
-          backgroundImage: `url('/img/newportri-2-.jpg')`,
-          backgroundPosition: `bottom`,
-          backgroundAttachment: `fixed`,
-        }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-          width: '75%'
-        }}
-      >
-        <h1 
-          className="is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-          sx={{
-            fontFamily: 'heading',
-            fontWeight: 'heading',
-          }}
-        >
-        Contact Us
-        </h1>
-        <h3
-          className="is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-          sx={{
-            fontWeight: "body",
-            fontFamily: "body",
-          }}
-        >
-        We know your time is valuable. That's why we promise to respond promptly to any questions you may have concerning our process. Reach out today and discover the infinite ways we can work together to make your business better.
-        </h3>
-      </div>
-    </div>
         <section className="section">
           <div className="container">
             <div className="content"
@@ -97,228 +48,9 @@ export default class Index extends React.Component {
               marginTop: 2,
               fontSize: 4,
             }}>
-              <div class="columns">
-                <div className='contactInformation column'>
-                  <h3
-                  sx={{
-                    margin: 4,
-                    marginTop: 0,
-                    fontWeight: "body",
-                    fontFamily: "heading",
-                  }}>Contact Form</h3>
-                    <a
-                      className="tile"
-                      sx={{
-                        margin: 4,
-                      }}
-                      href={`https://www.google.com.au/maps/search/${encodeURI(
-                       "9005 Overlook Blvd"
-                      )}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <MapPin 
-                      sx={{
-                      marginRight: 3,
-                      }}
-                      /> 
-                    <p 
-                    sx={{
-                      fontSize: 2,
-                      fontWeight: "body",
-                      fontFamily: "body",
-                    }}>9005 Overlook Blvd
-                    </p>
-                    </a>
-                    <a className="tile"
-                    href={`tel:${"+1-615-957-5603"}`}
-                    sx={{
-                      margin: 4,
-                    }}>
-                      <Smartphone 
-                      sx={{
-                      marginRight: 3,
-                      }}
-                      />
-                      <p
-                      sx={{
-                        fontSize: 2,
-                        fontWeight: "body",
-                        fontFamily: "body",
-                      }}>
-                       +1 (615) 601-0782
-                       </p>
-                    </a>
-                    <a className="tile"
-                    href={`mailto:${"hello@sprinterconsulting.com"}`}
-                    sx={{
-                      margin: 4,
-                    }}>
-                      <Mail 
-                      sx={{
-                      marginRight: 3,
-                      }}
-                        />
-                        <p 
-                        sx={{
-                          fontSize: 2,
-                          fontWeight: "body",
-                          fontFamily: "body",
-                        }}>
-                          hello@sprinterconsulting.com
-                        </p>
-                    </a>
-                    <iframe src="https://sprinter.substack.com/embed" width="480" height="320" frameborder="0" scrolling="no"></iframe>
+                <div className="column" sx={{minHeight: "25em", minWidth: "100%"}} >
+                  <ReactTypeformEmbed url="https://sprinter.typeform.com/to/Dq6veQ" />
                 </div>
-                <div className="column">
-                <Helmet>
-                  <script src="https://www.google.com/recaptcha/api.js" />
-                </Helmet>
-                    <form
-                      name="contact"
-                      method="post"
-                      action="/contact/thanks/"
-                      data-netlify="true"
-                      data-netlify-honeypot="bot-field"
-                      // data-netlify-recaptcha="true"
-                      onSubmit={this.handleSubmit}
-                    >
-                      {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                      <input type="hidden" name="form-name" value="contact" />
-                      <div hidden>
-                        <label>
-                          Don’t fill this out:{' '}
-                          <input name="bot-field" onChange={this.handleChange} />
-                        </label>
-                      </div>
-                      <div className="field">
-                        <label className="label" htmlFor={'name'}
-                        sx={{
-                          color: "text",
-                          fontWeight: "body",
-                          fontFamily: "body",
-                        }}>
-                          Your name
-                        </label>
-                        <div className="control">
-                          <input
-                            className="input"
-                            type={'text'}
-                            name={'name'}
-                            onChange={this.handleChange}
-                            id={'name'}
-                            required={true}
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <label className="label" htmlFor={'email'}
-                        sx={{
-                          color: 'text',
-                          fontWeight: "body",
-                          fontFamily: "body",
-                        }}>
-                          Email
-                        </label>
-                        <div className="control">
-                          <input
-                            className="input"
-                            type={'email'}
-                            name={'email'}
-                            onChange={this.handleChange}
-                            id={'email'}
-                            required={true}
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <label className="label" htmlFor={'enquiry'}
-                        sx={{
-                          color: 'text',
-                          fontWeight: "body",
-                          fontFamily: "body",
-                        }}>
-                          Type of Enquiry
-                        </label>
-                        <div className="control">
-                          <select 
-                            className="input"
-                            type={'enquiry'}
-                            name={'enquiry'}
-                            onChange={this.handleChange}
-                            id={'enquiry'}
-                            defaultValue="Select One"
-                            required={true}>
-                            <option disabled>
-                              Select One
-                            </option>
-                            <option>I would love to receive your posts in my email</option>
-                            <option>Digital Transformation scares me, can you help?</option>
-                            <option>I want to know more about working from anywhere</option>
-                            <option>Just want to say hello</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="field">
-                        <label className="label" htmlFor={'message'}
-                        sx={{
-                          color: 'text',
-                          fontWeight: "body",
-                          fontFamily: "body",
-                        }}>
-                          Message
-                        </label>
-                        <div className="control">
-                          <textarea
-                            className="textarea"
-                            name={'message'}
-                            onChange={this.handleChange}
-                            id={'message'}
-                            required={true}
-                          />
-                        </div>
-                      </div>
-                      <div className="field newsletter control">
-                          <input
-                            name={'newsletter'}
-                            type={'checkbox'}
-                            className="checkbox"
-                            onChange={this.handleChange}
-                            id={'newsletter'}
-                          />
-                          <span
-                          sx={{
-                            marginLeft: 2,
-                          }}
-                          >
-                            <label className="label" htmlFor={'newsletter'}
-                              sx={{
-                                color: 'text',
-                                fontWeight: "body",
-                                fontFamily: "body",
-                              }}>
-                              Get Updates (only the important stuff we promise)
-                            </label>
-                          </span>
-                      </div>
-                      {/* <div
-                        className="g-recaptcha"
-                        data-sitekey="6LekDb4UAAAAAK2fWolYytmkAxFYkzRdPaf76z-9"
-                        data-netlify-recaptcha="true"
-                      /> */}
-                      <div className="field">
-                        <button className="button is-link" type="submit"
-                        sx={{
-                          marginTop: 2,
-                          fontWeight: "body",
-                          fontFamily: "body",
-                        }}>
-                          Send
-                        </button>
-                      </div>
-                    </form>
-                    </div>
-            </div>
             </div>
           </div>
         </section>
