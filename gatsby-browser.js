9ded104725cf0efb364399413ea1352ea2690a04
Tyler Dreher <tyler@sprinterconsulting.com>
2020-05-18 12:08:09 -0500

    add segment npm package

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
 exports.onInitialClientRender = () => {
-    console.log("ReactDOM.render has executed")
       var s = document.createElement("script");
       s.type = "text/javascript";
       s.async = true;
@@ -7,8 +6,8 @@ exports.onInitialClientRender = () => {
       var x = document.getElementsByTagName('script')[0];
       x.parentNode.insertBefore(s, x);
 
-      var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var t=analytics.methods[e];analytics[t]=analytics.factory(t)}analytics.load=function(e,t){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+e+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=t};analytics.SNIPPET_VERSION="4.1.0";
-      analytics.load("eqCkHAFjderam3MgHYnznMtwN4jqkVp7");
-      analytics.page();
-      };
+    //   var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var t=analytics.methods[e];analytics[t]=analytics.factory(t)}analytics.load=function(e,t){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+e+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=t};analytics.SNIPPET_VERSION="4.1.0";
+    //   analytics.load("eqCkHAFjderam3MgHYnznMtwN4jqkVp7");
+    //   analytics.page();
+    //   };
   }
