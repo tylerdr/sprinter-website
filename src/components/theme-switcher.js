2339b3cb3be869f957d0ef61984cc3010cef8b69
Zach <zach@sprinterconsulting.com>
2020-05-21 18:58:14 -0500

    trying to deploy again

--- src/components/theme-switcher.js
+++ src/components/theme-switcher.js
@@ -11,27 +11,27 @@ import styled, { withTheme } from 'styled-components'
 const ThemeSwitcher = () => {
   const [colorMode, setColorMode] = useColorMode();
   const nextColorMode = colorMode === "light" ? "dark" : "light";
-  const gradient = colorMode === "light" ? "linear-gradient(#39598A, #79D7ED)" : "linear-gradient(#091236, #1E215D)"
-  const sunTransform = colorMode === "light" ? "translateY(0)": "translateY(100px)"
-  const moonTransform = colorMode === "light" ? "translateY(-100px)" : "translateY(0)"
-  const color = colorMode === "light" ? "#FDB813" : "#FEFCD7"
-  const containerBorder = colorMode === "light" ? "1px solid #FFF" : "1px solid #6B8096" 
+  const gradient = colorMode === "dark" ? "linear-gradient(#091236, #1E215D)" : "linear-gradient(#39598A, #79D7ED)"
+  const sunTransform = colorMode === "dark" ? "translateY(100px)" : "translateY(0)"
+  const moonTransform = colorMode === "dark" ? "translateY(0)": "translateY(-100px)"
+  const color = colorMode === "dark" ? "#FEFCD7": "#FDB813"
+  const containerBorder = colorMode === "dark" ?"1px solid #6B8096" : "1px solid #FFF"
   console.log(nextColorMode)
   return (
-    <button
+    <button 
+    className="toggle-mode"
         sx={{
         position: "absolute",
         top: 1,
-        right: 53,
-        borderRadius: "20px",
+        right: 50,
+        borderRadius: "30px",
         fontSize: "1.25em",
         justifyContent: "space-between",
         display: "flex",
         overflow: "hidden",
         margin: "0 auto",
         padding: "0.5rem",
-        width: "4rem",
-        height: "3rem",
+        cursor: "pointer",
         background : gradient,
         color: color,
         border: containerBorder
@@ -43,7 +43,7 @@ const ThemeSwitcher = () => {
         height: "auto",
         width: "2.5rem",
         transition: "all 0.3s linear",
-        transform: sunTransform
+        transform: sunTransform, 
       }} 
       icon={faSun}/>
       <FontAwesomeIcon 
