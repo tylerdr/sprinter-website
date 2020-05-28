2c11bb35c9702e9d088f93959903f3c3512f1a53
Zach <zach@sprinterconsulting.com>
2020-05-28 15:17:19 -0500

    fixing file path of icon for manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/img/SprinterFavicon.png`, // This path is relative to the root of the site.
+        icon: `${__dirname}/src/img/SprinterFavicon.png`, // This path is relative to the root of the site.
       },
     },
     {
